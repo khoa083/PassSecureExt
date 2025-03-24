f164fc3cb823eae1b8706351a1c42a8c124d4353
Kblack (Solo dev) <65351458+khoa083@users.noreply.github.com>
2025-03-24 23:29:54 +0700

    detailFrg

---
+++ app/src/main/java/com/kblack/passsecureext/fragments/details/DetailFragment.kt
@@ -0,0 +1,8 @@
+package com.kblack.passsecureext.fragments.details
+
+import androidx.fragment.app.Fragment
+import com.kblack.passsecureext.databinding.FragmentTestPasswordBinding
+
+class DetailFragment : Fragment() {
+    private lateinit var _binding: FragmentTestPasswordBinding
+}
