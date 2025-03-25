5bec20e17ad8d4bb37e5f2f7405869c96f0024af
Kblack (Solo dev) <65351458+khoa083@users.noreply.github.com>
2025-03-25 23:51:52 +0700

    upd GeneratePassphraseFragment

--- app/src/main/java/com/kblack/passsecureext/fragments/main/GeneratePassphraseFragment.kt
+++ app/src/main/java/com/kblack/passsecureext/fragments/main/GeneratePassphraseFragment.kt
@@ -1,27 +1,38 @@
-package com.kblack.passsecureext.fragments.details
+package com.kblack.passsecureext.fragments.main
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.kblack.passsecureext.databinding.FragmentTestPasswordBinding
+import com.kblack.passsecureext.databinding.FragmentGeneratePassphraseBinding
+import com.kblack.passsecureext.preferences.PreferenceManager
+import org.koin.android.ext.android.inject
 
-class DetailFragment : Fragment() {
-    private var _binding: FragmentTestPasswordBinding? = null
+class GeneratePassphraseFragment : Fragment() {
+
+    private var _binding: FragmentGeneratePassphraseBinding? = null
     private val fragmentBinding get() = _binding!!
+    private val preManager by inject<PreferenceManager>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentTestPasswordBinding.inflate(inflater, container, false)
+    ): View? {
+        _binding = FragmentGeneratePassphraseBinding.inflate(inflater, container, false)
         return fragmentBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        preManager.apply {
+
+        }
     }
 
     override fun onDestroyView() {
