7b2ab0a267fb2a75e223e7d6cab4489959cb4066
khoa083 <minhkhoa065@gmail.com>
2025-03-26 09:46:05 +0700

    upd models

---
+++ app/src/main/java/com/kblack/passsecureext/models/License.kt
@@ -0,0 +1,7 @@
+package com.kblack.passsecureext.models
+
+data class License(
+    val title: String,
+    val desc: String,
+    val url: String
+)
---
+++ app/src/main/java/com/kblack/passsecureext/models/SupportMethod.kt
@@ -0,0 +1,8 @@
+package com.kblack.passsecureext.models
+
+data class SupportMethod(
+    val title: String,
+    val titleIcon: Int,
+    val qr: Int,
+    val url: String
+)
