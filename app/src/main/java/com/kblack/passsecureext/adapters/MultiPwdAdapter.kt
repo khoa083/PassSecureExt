7c71be96d60045faa42540d880d2978de883bf46
khoa083 <minhkhoa065@gmail.com>
2025-03-27 17:18:40 +0700

    update adapter

--- app/src/main/java/com/kblack/passsecureext/adapters/MultiPwdAdapter.kt
+++ app/src/main/java/com/kblack/passsecureext/adapters/MultiPwdAdapter.kt
@@ -1,4 +1,56 @@
 package com.kblack.passsecureext.adapters
 
-class MultiPwdAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.kblack.passsecureext.R
+import com.kblack.passsecureext.models.MultiPwdItem
+import me.stellarsand.android.fastscroll.PopupTextProvider
+
+class MultiPwdAdapter(
+    private val aListViewItems: List<MultiPwdItem>,
+    private val clickListener: OnItemClickListener
+): RecyclerView.Adapter<MultiPwdAdapter.ListViewHolder>(), PopupTextProvider {
+
+    interface OnItemClickListener {
+        fun onItemClick(p: Int)
+    }
+
+    inner class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+
+        val passwordLine: TextView = itemView.findViewById(R.id.password_line)
+
+        init {
+            // Handle click events of items
+            itemView.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+            val p = bindingAdapterPosition
+            if(p != RecyclerView.NO_POSITION) clickListener.onItemClick(p)
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_file_recycler_view, parent, false)
+        return ListViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        holder.passwordLine.text = aListViewItems[position].passwordLine
+    }
+
+    override fun getItemCount(): Int = aListViewItems.size
+
+    override fun getItemViewType(position: Int): Int = position
+
+    override fun getPopupText(view: View, position: Int): CharSequence {
+        return aListViewItems[position].passwordLine.first().let {
+            if (it.isLowerCase()) it.uppercase()
+            else it
+        }.toString()
+    }
 }
--- app/src/main/java/com/kblack/passsecureext/adapters/SupportMethodItemAdapter.kt
+++ app/src/main/java/com/kblack/passsecureext/adapters/SupportMethodItemAdapter.kt
@@ -1,4 +1,51 @@
 package com.kblack.passsecureext.adapters
 
-class SupportMethodItemAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.imageview.ShapeableImageView
+import com.google.android.material.textview.MaterialTextView
+import com.kblack.passsecureext.R
+import com.kblack.passsecureext.activities.MainActivity
+import com.kblack.passsecureext.models.SupportMethod
+import com.kblack.passsecureext.utils.IntentUtils.Companion.openURL
+
+class SupportMethodItemAdapter(
+    private val aListViewItems: ArrayList<SupportMethod>,
+    private val mainActivity: MainActivity
+): RecyclerView.Adapter<SupportMethodItemAdapter.ListViewHolder>() {
+
+    inner class ListViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        val supportMethodTitle: MaterialTextView = itemView.findViewById(R.id.supportMethodTitle)
+        val supportMethodQR: ShapeableImageView = itemView.findViewById(R.id.supportMethodQr)
+        val supportMethodUrl: MaterialTextView = itemView.findViewById(R.id.supportMethodUrl)
+    }
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder
+    = ListViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_support_methods_recycler_view, parent, false))
+
+    override fun getItemCount(): Int = aListViewItems.size
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+
+        val supportMethod = aListViewItems[position]
+
+        holder.supportMethodTitle.apply {
+            text = supportMethod.title
+            setCompoundDrawablesWithIntrinsicBounds(supportMethod.titleIcon, 0, 0, 0)
+        }
+
+        holder.supportMethodQR.setImageResource(supportMethod.qr)
+
+        holder.supportMethodUrl.apply {
+            text = supportMethod.url
+            setOnClickListener{
+                openURL(mainActivity, supportMethod.url)
+            }
+        }
+
+    }
 }
