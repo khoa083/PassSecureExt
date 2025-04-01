45f6e7d6f77bf4f122feaf309b0dc3021e46af40
Kblack (Solo dev) <65351458+khoa083@users.noreply.github.com>
2025-04-01 22:53:40 +0700

    upd MultiPwdActivity

--- app/src/main/java/com/kblack/passsecureext/activities/MultiPwdActivity.kt
+++ app/src/main/java/com/kblack/passsecureext/activities/MultiPwdActivity.kt
@@ -5,14 +5,20 @@ import android.os.PersistableBundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.MenuProvider
 import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.kblack.passsecureext.R
+import com.kblack.passsecureext.databinding.ActivityMultiPwdBinding
 import com.kblack.passsecureext.objects.MultiPwdList
 import com.kblack.passsecureext.preferences.PreferenceManager
+import com.kblack.passsecureext.preferences.PreferenceManager.Companion.BLOCK_SS
 import com.kblack.passsecureext.preferences.PreferenceManager.Companion.GRID_VIEW
 import com.kblack.passsecureext.preferences.PreferenceManager.Companion.SORT_ASC
+import com.kblack.passsecureext.utils.UiUtils.Companion.blockScreenshots
 import com.kblack.passsecureext.utils.UiUtils.Companion.setNavBarContrastEnforced
 import org.koin.android.ext.android.inject
 
@@ -28,14 +34,57 @@ class MultiPwdActivity : AppCompatActivity(), MenuProvider {
         window.setNavBarContrastEnforced()
         super.onCreate(savedInstanceState)
         addMenuProvider(this)
+        onBackPressedDispatcher.addCallback(this, onBackPressedCallback)
+        val activityBinding = ActivityMultiPwdBinding.inflate(layoutInflater)
+        setContentView(activityBinding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.multi_pwd_nav_host) as NavHostFragment
+        navController = navHostFragment.navController
+        isGridView = prefManager.getBoolean(GRID_VIEW, defValue = false)
+        isAscSort = prefManager.getBoolean(SORT_ASC)
+
+        // Disable screenshots and screen recordings
+        window.blockScreenshots(prefManager.getBoolean(BLOCK_SS))
+
+        activityBinding.multiPwdBottomAppBar.apply {
+            setSupportActionBar(this)
+            setNavigationOnClickListener { onBackPressedDispatcher.onBackPressed() }
+        }
     }
 
     override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
-        TODO("Not yet implemented")
+        menuInflater.inflate(R.menu.menu_multi_pwd, menu)
+        menu.findItem(R.id.menu_view).apply {
+            if (!isGridView) setIcon(R.drawable.ic_view_grid)
+            else setIcon(R.drawable.ic_view_list)
+        }
     }
 
     override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
-        TODO("Not yet implemented")
+
+        when (menuItem.itemId) {
+
+            R.id.menu_view -> {
+                isGridView = !isGridView
+                if (!isGridView) menuItem.setIcon(R.drawable.ic_view_grid)
+                else menuItem.setIcon(R.drawable.ic_view_list)
+                navController.navigate(R.id.action_multiPwdFragment_self)
+            }
+
+            R.id.menu_sort -> {
+                isAscSort = !isAscSort
+                navController.navigate(R.id.action_multiPwdFragment_self)
+            }
+
+        }
+
+    }
+
+    private val onBackPressedCallback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            finish()
+        }
+
     }
 
     override fun onDestroy() {
