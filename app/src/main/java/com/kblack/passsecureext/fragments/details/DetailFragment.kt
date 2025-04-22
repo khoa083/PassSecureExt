a7c7eb2b6792245e6855835f8a7ecb76b5bce370
Kblack (Solo dev) <65351458+khoa083@users.noreply.github.com>
2025-04-23 00:05:07 +0700

    upd frag detail

--- app/src/main/java/com/kblack/passsecureext/fragments/details/DetailFragment.kt
+++ app/src/main/java/com/kblack/passsecureext/fragments/details/DetailFragment.kt
@@ -1,5 +1,6 @@
 package com.kblack.passsecureext.fragments.details
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,10 +8,18 @@ import android.view.ViewGroup
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
+import androidx.core.view.updateLayoutParams
 import androidx.core.view.updatePadding
 import androidx.fragment.app.Fragment
+import com.kblack.passsecureext.R
 import com.kblack.passsecureext.activities.DetailsActivity
+import com.kblack.passsecureext.common.EvaluatePassword
 import com.kblack.passsecureext.databinding.FragmentTestPasswordBinding
+import com.kblack.passsecureext.utils.ResultUtils
+import com.kblack.passsecureext.utils.UiUtils.Companion.convertDpToPx
+import com.nulabinc.zxcvbn.Zxcvbn
+import org.koin.android.ext.android.get
 
 class DetailFragment : Fragment() {
     private var _binding: FragmentTestPasswordBinding? = null
@@ -25,6 +34,7 @@ class DetailFragment : Fragment() {
         return fragmentBinding.root
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
         val password = (requireActivity() as DetailsActivity).passwordLine
@@ -36,7 +46,37 @@ class DetailFragment : Fragment() {
             v.updatePadding(left = insets.left, right = insets.right, bottom = insets.bottom)
             WindowInsetsCompat.CONSUMED
         }
+        ViewCompat.setOnApplyWindowInsetsListener(fragmentBinding.passwordBox) { v, windowInsets ->
+            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars()
+                                                        or WindowInsetsCompat.Type.displayCutout())
+            v.updatePadding(left = insets.left, right = insets.right)
+            v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                topMargin = insets.top + convertDpToPx(requireContext(), 12f)
+            }
+            WindowInsetsCompat.CONSUMED
+        }
 
+        fragmentBinding.apply {
+            lengthSubtitle.text = "\u2022 ${getString(R.string.length)}"
+            uppercaseSubtitle.text = "\u2022 ${getString(R.string.uppercase)}"
+            lowercaseSubtitle.text = "\u2022 ${getString(R.string.lowercase)}"
+            numbersSubtitle.text = "\u2022 ${getString(R.string.numbers)}"
+            specialCharsSubtitle.text = "\u2022 ${getString(R.string.special_char)}"
+            spacesSubtitle.text = "\u2022 ${getString(R.string.spaces)}"
+            testMultipleFab.isVisible = false
+            passwordText.apply {
+                setText(password)
+                isFocusable = false
+                isCursorVisible = false
+            }
+        }
+
+        EvaluatePassword(zxcvbn = get<Zxcvbn>(),
+            password = password,
+            fragmentBinding = fragmentBinding,
+            context = requireContext(),
+            resultUtils = ResultUtils(requireContext())
+        )
 
     }
 
