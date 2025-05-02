a3ffce34404b64403ad87902344c9b8a2fb3e0e7
Kblack (Solo dev) <65351458+khoa083@users.noreply.github.com>
2025-05-02 23:25:37 +0700

    upd GeneratePasswordFragment

--- app/src/main/java/com/kblack/passsecureext/fragments/main/GeneratePasswordFragment.kt
+++ app/src/main/java/com/kblack/passsecureext/fragments/main/GeneratePasswordFragment.kt
@@ -1,18 +1,46 @@
 package com.kblack.passsecureext.fragments.main
 
+import android.annotation.SuppressLint
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context.CLIPBOARD_SERVICE
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updateLayoutParams
+import androidx.core.view.updatePadding
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import com.google.android.material.materialswitch.MaterialSwitch
+import com.google.android.material.slider.Slider
+import com.kblack.passsecureext.R
+import com.kblack.passsecureext.activities.DetailsActivity
 import com.kblack.passsecureext.activities.MainActivity
 import com.kblack.passsecureext.databinding.FragmentGeneratePasswordBinding
 import com.kblack.passsecureext.preferences.PreferenceManager
+import com.kblack.passsecureext.preferences.PreferenceManager.Companion.PWD_AMB_CHARS
+import com.kblack.passsecureext.preferences.PreferenceManager.Companion.PWD_LENGTH
+import com.kblack.passsecureext.preferences.PreferenceManager.Companion.PWD_LOWERCASE
+import com.kblack.passsecureext.preferences.PreferenceManager.Companion.PWD_NUMBERS
+import com.kblack.passsecureext.preferences.PreferenceManager.Companion.PWD_SPACES
+import com.kblack.passsecureext.preferences.PreferenceManager.Companion.PWD_SPEC_CHARS
+import com.kblack.passsecureext.preferences.PreferenceManager.Companion.PWD_UPPERCASE
+import com.kblack.passsecureext.utils.ClipboardUtils.Companion.hideSensitiveContent
+import com.kblack.passsecureext.utils.UiUtils.Companion.convertDpToPx
+import com.kblack.passsecureext.utils.UiUtils.Companion.showSnackBar
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.koin.android.ext.android.inject
 import java.security.SecureRandom
 
 class GeneratePasswordFragment : Fragment() {
+
     private var _binding: FragmentGeneratePasswordBinding? = null
     private val fragmentBinding get() = _binding!!
     private val prefManager by inject<PreferenceManager>()
@@ -34,33 +62,220 @@ class GeneratePasswordFragment : Fragment() {
         private const val LOWERCASE_CHARS = "abcdefghijklmnopqrstuvwxyz"
         private const val NUMBERS = "0123456789"
         private const val SPECIAL_CHARS = "!@#$%^&*+_-.="
-        private const val UPPERCASE_AMB_CHARS = "ILOSBZ"
+        private const val UPPERCASE_AMB_CHARS = "ILOSBZ" // 0Oo, 1IlL, 2Z, 5S, 8B
         private const val LOWERCASE_AMB_CHARS = "loz"
         private const val NUM_AMB_CHARS = "01258"
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        // Inflate the layout for this fragment
         _binding = FragmentGeneratePasswordBinding.inflate(inflater, container, false)
         return fragmentBinding.root
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
         val mainActivity = requireActivity() as MainActivity
+        uppercaseSwitch = fragmentBinding.uppercaseSwitch
+        lowercaseSwitch = fragmentBinding.lowercaseSwitch
+        numbersSwitch = fragmentBinding.numbersSwitch
+        specialCharsSwitch = fragmentBinding.specialCharsSwitch
+        avoidAmbCharsSwitch = fragmentBinding.avoidAmbCharsSwitch
+        includeSpaceSwitch = fragmentBinding.includeSpacesSwitch
+
+        primarySwitchesList = arrayOf(uppercaseSwitch,
+            lowercaseSwitch,
+            numbersSwitch,
+            specialCharsSwitch)
+
+        primarySwitchesPrefMap = mapOf(uppercaseSwitch to PWD_UPPERCASE,
+            lowercaseSwitch to PWD_LOWERCASE,
+            numbersSwitch to PWD_NUMBERS,
+            specialCharsSwitch to PWD_SPEC_CHARS)
+
+        // Adjust scrollview for edge to edge
+        ViewCompat.setOnApplyWindowInsetsListener(fragmentBinding.pwdScrollView) { v, windowInsets ->
+            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars()
+                    or WindowInsetsCompat.Type.displayCutout())
+            v.updatePadding(left = insets.left, top = insets.top, right = insets.right)
+            v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                bottomMargin = insets.bottom + convertDpToPx(requireContext(), 64f)
+            }
+            WindowInsetsCompat.CONSUMED
+        }
+
+        // Password length slider
+        fragmentBinding.pwdLengthSlider.apply {
+            value = prefManager.getFloat(PWD_LENGTH)
+            fragmentBinding.pwdLengthText.text = "${getString(R.string.length)}: ${value.toInt()}"
+
+            addOnSliderTouchListener(object : Slider.OnSliderTouchListener {
+                override fun onStartTrackingTouch(slider: Slider) {}
+
+                override fun onStopTrackingTouch(slider: Slider) {
+                    generatePassword()
+                }
+
+            })
+
+            addOnChangeListener { _, value, _ ->
+                fragmentBinding.pwdLengthText.text = "${getString(R.string.length)}: ${value.toInt()}"
+            }
+        }
+
+        // Switches
+        // At least one switch must be enabled at all times (excluding ambiguous chars switch)
+        primarySwitchesList.forEach { switch ->
+            primarySwitchesPrefMap[switch]?.let { preferenceKey ->
+                switch.isChecked = prefManager.getBoolean(preferenceKey)
+            }
+            switch.setOnCheckedChangeListener { _, _ ->
+                val otherSwitches = primarySwitchesList.filter { it != switch }
+                val otherSwitchesChecked = otherSwitches.any { it.isChecked }
+                if (otherSwitchesChecked) {
+                    generatePassword()
+                }
+                else {
+                    switch.isChecked = true
+                }
+            }
+        }
+
+        avoidAmbCharsSwitch.apply {
+            isChecked = prefManager.getBoolean(PWD_AMB_CHARS)
+            setOnCheckedChangeListener { _, _ ->
+                generatePassword()
+            }
+        }
+
+        includeSpaceSwitch.apply {
+            isChecked = prefManager.getBoolean(PWD_SPACES, defValue = false)
+            setOnCheckedChangeListener { _, _ ->
+                generatePassword()
+            }
+        }
+
+        generatePassword()
+
+        // Details
+        fragmentBinding.pwdDetailsBtn.setOnClickListener {
+            startActivity(Intent(requireActivity(), DetailsActivity::class.java)
+                .putExtra("PwdLine", fragmentBinding.pwdGeneratedTextView.text))
+        }
+
+        // Copy
+        fragmentBinding.pwdCopyBtn.setOnClickListener {
+            val clipData = ClipData.newPlainText("", fragmentBinding.pwdGeneratedTextView.text)
+            if (Build.VERSION.SDK_INT >= 24) clipData.hideSensitiveContent()
+            (requireContext().getSystemService(CLIPBOARD_SERVICE) as ClipboardManager).setPrimaryClip(clipData)
+            // Only show snackbar in 12L or lower to avoid duplicate notifications
+            // https://developer.android.com/develop/ui/views/touch-and-input/copy-paste#duplicate-notifications
+            if (Build.VERSION.SDK_INT <= 32) {
+                showSnackBar(mainActivity.activityBinding.mainCoordLayout,
+                    requireContext().getString(R.string.copied_to_clipboard),
+                    mainActivity.activityBinding.mainBottomNav)
+            }
+        }
+
+        // Regenerate
+        fragmentBinding.pwdRegenerateBtn.setOnClickListener {
+            generatePassword()
+        }
+
+        // Share
+        fragmentBinding.pwdShareBtn.setOnClickListener {
+            startActivity(Intent.createChooser(Intent(Intent.ACTION_SEND)
+                .setType("text/plain")
+                .putExtra(Intent.EXTRA_TEXT, fragmentBinding.pwdGeneratedTextView.text),
+                getString(R.string.share)))
+        }
+
+    }
+
+    private fun getNonAmbChars(allChars: String, ambChars: String): String {
+        return allChars.filterNot { it in ambChars }
+    }
+
+    private fun generatePassword() {
+        lifecycleScope.launch(Dispatchers.Default) {
+            val allChars = buildString {
+                if (uppercaseSwitch.isChecked) {
+                    append(
+                        if (avoidAmbCharsSwitch.isChecked) uppercaseWithoutAmbChars.ifEmpty {
+                            // Only generate non-ambiguous chars if not done already.
+                            // This would avoid unnecessary generations everytime this function is called.
+                            getNonAmbChars(UPPERCASE_CHARS,
+                                UPPERCASE_AMB_CHARS).also { uppercaseWithoutAmbChars = it }
+                        }
+                        else UPPERCASE_CHARS
+                    )
+                }
+                if (lowercaseSwitch.isChecked) {
+                    append(
+                        if (avoidAmbCharsSwitch.isChecked) lowercaseWithoutAmbChars.ifEmpty {
+                            getNonAmbChars(LOWERCASE_CHARS,
+                                LOWERCASE_AMB_CHARS).also { lowercaseWithoutAmbChars = it }
+                        }
+                        else LOWERCASE_CHARS
+                    )
+                }
+                if (numbersSwitch.isChecked) {
+                    append(
+                        if (avoidAmbCharsSwitch.isChecked) numbersWithoutAmbChars.ifEmpty {
+                            getNonAmbChars(NUMBERS, NUM_AMB_CHARS).also {
+                                numbersWithoutAmbChars = it
+                            }
+                        }
+                        else NUMBERS
+                    )
+                }
+                if (specialCharsSwitch.isChecked) append(SPECIAL_CHARS)
+            }
+
+            val password = buildString {
+                val length = fragmentBinding.pwdLengthSlider.value.toInt()
+                val maxSpaces = (length * 0.2).coerceAtMost(15.0).toInt()
+
+                for (i in 0 until length) {
+                    if (includeSpaceSwitch.isChecked
+                        && i in 1 until (length - 1) // Avoid spaces at the beginning & end
+                        && secureRandom.nextInt(length + 1) < maxSpaces
+                        && this[i - 1] != ' ') {
+                        append(' ')
+                    }
+                    else {
+                        val randomIndex = secureRandom.nextInt(allChars.length)
+                        append(allChars[randomIndex])
+                    }
+                }
+            }
+
+            withContext(Dispatchers.Main) {
+                fragmentBinding.pwdGeneratedTextView.text = password
+            }
+        }
     }
 
     override fun onPause() {
         super.onPause()
+        prefManager.apply {
+            setFloat(PWD_LENGTH, fragmentBinding.pwdLengthSlider.value)
+            primarySwitchesList.forEach { switch ->
+                primarySwitchesPrefMap[switch]?.let { preferenceKey ->
+                    setBoolean(preferenceKey, switch.isChecked)
+                }
+            }
+            setBoolean(PWD_AMB_CHARS, fragmentBinding.avoidAmbCharsSwitch.isChecked)
+            setBoolean(PWD_SPACES, fragmentBinding.includeSpacesSwitch.isChecked)
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-
 
 }
